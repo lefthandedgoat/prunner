fa49c584980834aec352776f6c7a7f1d7d1f7b95
Chris Holt <lefthandedgoat@gmail.com>
2016-02-19 09:55:17 -0600

    initial

---
+++ AssemblyInfo.fs
@@ -0,0 +1,12 @@
+﻿namespace System
+open System.Reflection
+
+[<assembly: AssemblyTitleAttribute("prunner")>]
+[<assembly: AssemblyProductAttribute("prunner")>]
+[<assembly: AssemblyDescriptionAttribute("Super simple f# parallel unit test library")>]
+[<assembly: AssemblyVersionAttribute("1.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.0")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "1.0"
---
+++ Program.fs
@@ -0,0 +1,3 @@
+module Main
+
+let a = 1
